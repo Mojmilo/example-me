4057a9c44aa5cad7d1312a2109b6c07b78932a7f
MaxVzl <maxime.vozelle@gmail.com>
2023-02-17 21:12:47 +0100

    send

--- server/src/App/App.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var HttpServer_1 = require("../Services/Server/HttpServer");
-var BaseController_1 = require("../Services/Controller/BaseController");
-var UserController_1 = require("../Services/Controller/UserController");
-HttpServer_1.HttpServer.getInstance();
-BaseController_1.BaseController.getInstance();
-UserController_1.UserController.getInstance();
-//# sourceMappingURL=App.js.map
--- server/src/Services/Server/SocketServer.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=SocketServer.js.map
